8edfb542b00c7b71a633f940f48620de2bd6e682
Skip Montanaro <skip.montanaro@gmail.com>
2025-06-17 14:53:15 -0500

    actually exercise some ymcacals code

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,12 @@
 # simple no-op test so tox doesn't keep complaining until I get real tests...
+
+from pathlib import Path
+
+import icalendar
+
+from ymcacals.ymcacals import CalendarMerger
+
 def test_basic():
-    assert True
+    ics_path = Path(__file__).parent / "skip.ics"
+    calendar = icalendar.Calendar.from_ical(ics_path.read_bytes())
+    assert calendar.events
