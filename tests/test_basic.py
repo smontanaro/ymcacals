a883f2ec92b4100b727aa664e82cc63e054bc235
Skip Montanaro <skip.montanaro@gmail.com>
2025-06-18 21:07:38 -0500

    wait for http server to start

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,13 +6,22 @@ import sys
 import time
 
 import icalendar
+import requests
 
 from ymcacals.ymcacals import CalendarMerger
 
 def test_basic():
     server = subprocess.Popen([sys.executable, "-m", "http.server", "-d", "./tests"])
-    # give server time to start
-    time.sleep(0.25)
+    for _i in range(10):
+        # give server time to start
+        time.sleep(0.25)
+        try:
+            x = requests.get("http://localhost:8000/skip.ics")
+        except requests.exceptions.ConnectionError:
+            continue
+        break
+    else:
+        raise requests.exceptions.ConnectionError("Too many failed connection attempts")
     try:
         merger = CalendarMerger(Path(__file__).parent / "skip.csv", True)
         merger.verbose = False
